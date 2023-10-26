60ce12d7a13afecd4b67d022aec4547dc668447b
Zach <49828953+AeroSouls@users.noreply.github.com>
2023-10-26 17:18:25 -0400

    update

--- winutil.ps1
+++ winutil.ps1
@@ -1295,6 +1295,7 @@ Function Invoke-WPFFormVariables {
     Write-Host "=====Windows Toolbox====="
     Write-Host "" 
 
+
     #====DEBUG GUI Elements====
 
     #Write-Host "Found the following interactable elements from our form" -ForegroundColor Cyan
